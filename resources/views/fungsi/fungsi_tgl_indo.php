eec2a85699879c6dc4bb1019255aaa98427ebcea
Gilang Risky M <gilangriskymahardhika@gmail.com>
2025-04-16 12:50:57 +0700

    tambah enter bagian fungsi

--- resources/views/fungsi/fungsi_tgl_indo.php
+++ resources/views/fungsi/fungsi_tgl_indo.php
@@ -2,53 +2,56 @@
 function tgl_indonesia($date)
 {
     // ARRAY untuk hari dan bulan //
-    $Hari = array("Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu",);
+    $Hari = array("Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", );
     $Bulan = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
 
     // Memisahkan format tanggal bulan dan tahun menggunakan substring//
-    $tahun      = substr($date, 0, 4);
-    $bulan      = substr($date, 5, 2);
-    $tgl     = substr($date, 8, 2);
-    $hari     = date("w", strtotime($date));
+    $tahun = substr($date, 0, 4);
+    $bulan = substr($date, 5, 2);
+    $tgl = substr($date, 8, 2);
+    $hari = date("w", strtotime($date));
 
-    $result = $Hari[$hari] . ", " . $tgl . " " . $Bulan[(int)$bulan - 1] . " " . $tahun . "";
+    $result = $Hari[$hari] . ", " . $tgl . " " . $Bulan[(int) $bulan - 1] . " " . $tahun . "";
     return $result;
 }
+
 function tgl_indonesia2($date)
 {
     // ARRAY untuk hari dan bulan //
-    $Hari = array("Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu",);
+    $Hari = array("Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", );
     $Bulan = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
 
     // Memisahkan format tanggal bulan dan tahun menggunakan substring//
-    $tahun      = substr($date, 0, 4);
-    $bulan      = substr($date, 5, 2);
-    $tgl     = substr($date, 8, 2);
-    $hari     = date("w", strtotime($date));
+    $tahun = substr($date, 0, 4);
+    $bulan = substr($date, 5, 2);
+    $tgl = substr($date, 8, 2);
+    $hari = date("w", strtotime($date));
 
-    $result = $Hari[$hari] . " " . $tgl . " " . $Bulan[(int)$bulan - 1] . " " . $tahun . "";
+    $result = $Hari[$hari] . " " . $tgl . " " . $Bulan[(int) $bulan - 1] . " " . $tahun . "";
     return $result;
 }
+
 function tgl_indonesia3($date)
 {
     // ARRAY untuk hari dan bulan //
     $Bulan = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
 
     // Memisahkan format tanggal bulan dan tahun menggunakan substring//
-    $tahun      = substr($date, 0, 4);
-    $bulan      = substr($date, 5, 2);
-    $tgl     = substr($date, 8, 2);
+    $tahun = substr($date, 0, 4);
+    $bulan = substr($date, 5, 2);
+    $tgl = substr($date, 8, 2);
 
-    $result = $tgl . " " . $Bulan[(int)$bulan - 1] . " " . $tahun . "";
+    $result = $tgl . " " . $Bulan[(int) $bulan - 1] . " " . $tahun . "";
     return $result;
 }
+
 function tgl_indonesia4($date)
 {
     // ARRAY untuk hari //
-    $Hari = array("Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu",);
+    $Hari = array("Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", );
 
     // Memisahkan format hari menggunakan strtotime//
-    $hari     = date("w", strtotime($date));
+    $hari = date("w", strtotime($date));
 
     $result = $Hari[$hari];
     return $result;
